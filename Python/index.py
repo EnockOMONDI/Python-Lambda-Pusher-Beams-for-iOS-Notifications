857750e3a2b233a41413bf6b3e5af0fd9c8233b2
Christopher Batin <chris@batin.me>
2019-04-02 20:51:19 +0100

    Updated

--- Python/index.py
+++ Python/index.py
@@ -3,8 +3,8 @@ import requests
 
 def lambda_handler(event, context):
     beams_client = PushNotifications(
-                instance_id='0fb4a756-8780-4425-b393-3c7855470d99',
-                secret_key='7A57DBC7C435D04039C301492C05F75F9AD65835D5DCF41F50FC901F68AA93C2',
+                instance_id='YOUR_INSTANCE_ID',
+                secret_key='YOUR_SECRET_KEY',
                 )
 
     response = beams_client.publish_to_interests(interests=['hello'],
